b2a958f46fa638f4340c7e0a2282c0fce259d836
redbyte09 <sakshisharma4234@gmail.com>
2025-02-27 12:50:27 +0530

    [Sakshi] Add . Determine the Logging Levels, Logging to Console or File, Logging Patterns, etc based on this application running in Dev, Staging or Production.

--- employeepayrollapp/src/main/java/com/capgemini/employeepayrollapp/validation/EmployeeNotFoundException.java
+++ employeepayrollapp/src/main/java/com/capgemini/employeepayrollapp/validation/EmployeeNotFoundException.java
@@ -1,7 +1,6 @@
 package com.capgemini.employeepayrollapp.validation;
-
 public class EmployeeNotFoundException extends RuntimeException {
     public EmployeeNotFoundException(String message) {
         super(message);
-    }
+}
 }
