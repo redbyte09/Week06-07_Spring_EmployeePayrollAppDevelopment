d0e4fd8e38cbdd891355064c4027084ad0d2831e
redbyte09 <sakshisharma4234@gmail.com>
2025-02-27 16:20:11 +0530

    '

--- employeepayrollapp/src/main/java/com/capgemini/employeepayrollapp/exception/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.employeepayrollapp.exception;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-}
